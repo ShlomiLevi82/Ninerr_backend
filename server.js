8c95abe726a9bf8aeea6c57e32d95390ef26dcb5
Shlomi Levi <shlomilevi82@gmail.com>
2023-08-06 12:59:55 +0300

    .

--- server.js
+++ server.js
@@ -18,8 +18,10 @@ if (process.env.NODE_ENV === 'production') {
     origin: [
       'http://127.0.0.1:3000',
       'http://localhost:3000',
-      'http://127.0.0.1:5173',
       'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'http://127.0.0.1:5174',
+      'http://localhost:5174',
     ],
     credentials: true,
   }
